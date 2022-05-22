7fdca25447208f7fac931f647b28ddede9aa0d56
daraphista <daraphistadaone@gmail.com>
2022-05-22 10:20:04 +0800

    feat(storeProject): storeProject test

--- src/tests/Project.test.js
+++ src/tests/Project.test.js
@@ -1,1 +1,17 @@
-test.todo("")
+import Project, { storeProject, deleteProject, editProject } from "../js/Project";
+
+let newProject;
+
+beforeEach(() => {
+  newProject = Project("House chores");
+})
+
+describe("projects", () => {
+  test("can be stored", () => {
+    const projectList = [];
+    storeProject(newProject, projectList);
+    expect(projectList).toContain(newProject);
+  });
+  test.todo("can be deleted");
+  test.todo("can be edited");
+});
