23674e7c6d183f7f9089e14c34b4d4ddaded7a4b
daraphista <daraphistadaone@gmail.com>
2022-05-23 14:09:21 +0800

    feat(deleteProject): successful implementation

---
+++ src/js/Project.js
@@ -0,0 +1,19 @@
+const Project = (title, tasklist = []) => {
+  return { title, tasklist };
+};
+
+const storeProject = (project, array) => {
+  array.push(project);
+}
+
+const deleteProject = (project, array) => {
+  const projectIndex = array.indexOf(project);
+  array.splice(projectIndex);
+}
+
+const editProject = (project, property, value) => {
+  
+}
+
+export default Project;
+export { storeProject, deleteProject, editProject };
