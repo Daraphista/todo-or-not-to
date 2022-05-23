50c397961afe60fcf87bfbf2b6e8c35926a4e2fe
daraphista <daraphistadaone@gmail.com>
2022-05-23 16:21:54 +0800

    feat(firebase): initialize firestore

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBJOoRvmW5p3Z0ojgH8TU3E5aQ52NRdxuw",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
