a0146332e9b17ab0608c910cc4f0087c62a1bedb
daraphista <daraphistadaone@gmail.com>
2022-05-23 16:21:22 +0800

    feat(firebase): initialize firebase app

---
+++ src/js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJOoRvmW5p3Z0ojgH8TU3E5aQ52NRdxuw",
+  authDomain: "todo-or-not-to.firebaseapp.com",
+  projectId: "todo-or-not-to",
+  storageBucket: "todo-or-not-to.appspot.com",
+  messagingSenderId: "659746874836",
+  appId: "1:659746874836:web:1c51f5af18f4b66b296241",
+  measurementId: "G-N2P5G75KEE"
+};
+
+const app = initializeApp(firebaseConfig);
