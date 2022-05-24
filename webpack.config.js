d8c537cd76fe966ec8d1c2c4a1ceab831733b6a5
daraphista <daraphistadaone@gmail.com>
2022-05-24 13:26:52 +0800

    fix(webpack): fix syntax error

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   devServer: {
     static: {
-      directory: path.join(__dirname, "public");
+      directory: path.join(__dirname, "public"),
     }
   },
   mode: "development",
