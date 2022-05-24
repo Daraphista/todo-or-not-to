b5c8aa6ae53801625989afabef7e8db60c462b62
daraphista <daraphistadaone@gmail.com>
2022-05-24 13:35:56 +0800

    fix(devServer): fix devServer

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ const path = require("path");
 module.exports = {
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"),
-    }
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000
   },
   mode: "development",
   entry: "./src/js/index.js",
