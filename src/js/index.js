80c191fbe90d6c02b7badeeeb77d69882b38fe95
daraphista <daraphistadaone@gmail.com>
2022-05-25 22:51:41 +0800

    feat(index.js): add sign up functionality

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,21 @@ signInForm.addEventListener("submit", (e) => {
   signIn(email, password);
 });
 
+// Sign Up functionality
+
+import { createUser } from "./firebase";
+
+const signUpForm = document.querySelector(".sign-up");
+
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const emailInput = document.querySelector(".sign-up #email");
+  const passwordInput = document.querySelector(".sign-up #password");
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  createUser(email, password);
+});
+
