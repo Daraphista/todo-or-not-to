f203ee2ce4f1a4223bd7956e1ac8e4ea86a0d707
daraphista <daraphistadaone@gmail.com>
2022-05-27 14:46:09 +0800

    feat(project tests): test createProject

--- src/tests/Project.test.js
+++ src/tests/Project.test.js
@@ -1,1 +1,23 @@
+import { addDoc } from "firebase/firestore";
+import { createProject } from "../js/Project";
 
+jest.mock("firebase/firestore");
+
+describe("Projects", () => {
+  describe("createProject", () => {
+    test("calls the addDoc() function", () => {
+      createProject();
+
+      expect(addDoc).toBeCalled();
+    });
+
+    test("calls the addDoc() function with the right arguments", () => {
+      const collection = {};
+      const title = "";
+
+      createProject(collection, title);
+
+      expect(addDoc).toHaveBeenCalledWith(collection, { title: title });
+    });
+  });
+});
