4813cd5a49f78a6ccb64d6222a817bea4adf0d9d
daraphista <daraphistadaone@gmail.com>
2022-05-27 14:47:25 +0800

    feat(project): create createProject function

--- src/js/Project.js
+++ src/js/Project.js
@@ -1,1 +1,11 @@
+// function that creates a project document in firestore
 
+import { addDoc } from "firebase/firestore";
+
+const createProject = (collection, title) => {
+  addDoc(collection, {
+    title: title,
+  });
+};
+
+export { createProject };
