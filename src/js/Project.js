d67440969040592e840a5e43648b8ba7fb42b5a4
daraphista <daraphistadaone@gmail.com>
2022-05-27 14:46:45 +0800

    fix(project): remove redundant functions

--- src/js/Project.js
+++ src/js/Project.js
@@ -1,19 +1,1 @@
-const Project = (title, tasklist = []) => {
-  return { title, tasklist };
-};
 
-const storeProject = (project, array) => {
-  array.push(project);
-};
-
-const deleteProject = (project, array) => {
-  const projectIndex = array.indexOf(project);
-  array.splice(projectIndex);
-};
-
-const editProject = (project, property, value) => {
-  project[property] = value;
-};
-
-export default Project;
-export { storeProject, deleteProject, editProject };
