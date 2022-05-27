bee15ea60f7e7490da13881bae78e4b2696da3ae
daraphista <daraphistadaone@gmail.com>
2022-05-27 11:37:18 +0800

    feat(user-data): format code

--- src/js/user-data.js
+++ src/js/user-data.js
@@ -1,14 +1,10 @@
 // Get user data
 
-import {
-  collection,
-  query,
-  onSnapshot,
-} from "firebase/firestore";
+import { collection, query, onSnapshot } from "firebase/firestore";
 
 const getUserData = (database, path, callback) => {
   const collectionQuery = query(collection(database, path));
   const unsub = onSnapshot(collectionQuery, callback);
 };
 
-export { getUserData }
+export { getUserData };
