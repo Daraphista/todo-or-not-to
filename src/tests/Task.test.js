228a5bfa2d1b5129d0d586ec3f46a58bf5fd6cab
daraphista <daraphistadaone@gmail.com>
2022-05-27 21:28:26 +0800

    feat(createTask): test if createTask calls addDoc()

--- src/tests/Task.test.js
+++ src/tests/Task.test.js
@@ -1,8 +1,13 @@
 import { createTask } from "../js/Task";
+import { addDoc } from "firebase/firestore";
+
+jest.mock("firebase/firestore");
 
 describe("Tasks", () => {
   describe("createTask", () => {
-    
-  })
-})
-
+    test("calls addDoc()", () => {
+      createTask();
+      expect(addDoc).toBeCalled();
+    });
+  });
+});
