df598ad504e41018934bcb0b4bbd7c0cbff5d61f
daraphista <daraphistadaone@gmail.com>
2022-05-27 21:28:53 +0800

    feat(Task): add createTask function

--- src/js/Task.js
+++ src/js/Task.js
@@ -1,1 +1,14 @@
+import { addDoc } from "firebase/firestore";
 
+const createTask = (collection, title, description, priority, date) => {
+  const newTask = {
+    title,
+    description,
+    priority,
+    date,
+    isFinished: false,
+  };
+  addDoc(collection, newTask);
+};
+
+export { createTask };
