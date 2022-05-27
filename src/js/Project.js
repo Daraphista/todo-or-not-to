1cc52cd7a3e1d5542d9aa08799f5b36881628085
daraphista <daraphistadaone@gmail.com>
2022-05-27 20:59:26 +0800

    feat(Project): delete subcollection of project with deleteProject

--- src/js/Project.js
+++ src/js/Project.js
@@ -1,6 +1,13 @@
 // function that creates a project document in firestore
 
-import { addDoc, collection, deleteDoc, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  query,
+  QuerySnapshot,
+  setDoc,
+} from "firebase/firestore";
 
 const createProject = (collection, title) => {
   addDoc(collection, {
@@ -20,7 +27,27 @@ export { editProject };
 
 // function that deletes a project and it's subcollections
 
+import { getUserData } from "./user-data";
+import { database } from "./firebase";
+import { doc } from "firebase/firestore";
+
+// function to manually delete subcollections of project document
+const deleteProjectSubcollections = (document) => {
+  const projectPath = document._key.path.segments.join("/");
+  const tasksCollectionPath = `${projectPath}/tasks`;
+
+  console.log(tasksCollectionPath);
+
+  getUserData(database, tasksCollectionPath, (querySnapshot) => {
+    querySnapshot.forEach((task) => {
+      const docRef = doc(database, tasksCollectionPath, task.id);
+      deleteDoc(docRef);
+    });
+  });
+};
+
 const deleteProject = (document) => {
+  deleteProjectSubcollections(document);
   deleteDoc(document);
 };
 
