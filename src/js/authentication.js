79af2328943f84019d3170866dc879ba5e81c37b
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-13 16:05:05 +0800

    fix(handleSignIn): move into Authentication object module

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -1,17 +1,4 @@
-// move sign in functionality here
-
 import { signIn } from "./firebase";
-
-const handleSignIn = (signInForm, emailInput, passwordInput) => {
-  signInForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const email = emailInput.value;
-    const password = passwordInput.value;
-
-    signIn(email, password);
-  });
-};
 
 import { createUser } from "./firebase";
 
@@ -20,6 +7,19 @@ import { signOut, getAuth } from "firebase/auth";
 // Authentication module object
 
 const Authentication = (() => {
+  // move sign in functionality here
+  
+  const handleSignIn = (signInForm, emailInput, passwordInput) => {
+    signInForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      
+      const email = emailInput.value;
+      const password = passwordInput.value;
+      
+      signIn(email, password);
+    });
+  };
+  
   // move sign up functionality here
   
   const handleSignUp = (signUpForm, emailInput, passwordInput) => {
