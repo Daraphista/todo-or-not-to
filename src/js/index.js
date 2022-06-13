9d4b15e968f616c5400148e5669859e07149726a
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-13 15:59:18 +0800

    fix(index): remove authentication functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,5 @@
 import "../style/style.css";
 
-// Sign In functionality
-
-import { handleSignIn } from "./authentication";
-
-const signInForm = {
-  signInForm: document.querySelector(".sign-in"),
-  emailInput: document.querySelector(".sign-in #email"),
-  passwordInput: document.querySelector(".sign-in #password"),
-};
-
-handleSignIn(
-  signInForm.signInForm,
-  signInForm.emailInput,
-  signInForm.passwordInput
-);
-
-// Sign Up functionality
-
-import { handleSignUp } from "./authentication";
-
-const signUpForm = {
-  signUpForm: document.querySelector(".sign-up"),
-  emailInput: document.querySelector(".sign-up #email"),
-  passwordInput: document.querySelector(".sign-up #password"),
-};
-
-handleSignUp(
-  signUpForm.signUpForm,
-  signUpForm.emailInput,
-  signUpForm.passwordInput
-);
-
-// Log out functionality
-
-import { handleSignOut } from "./authentication";
-
-const logOutButton = document.querySelector(".logout");
-
-handleSignOut(logOutButton);
-
 // Get user data when signed in
 
 import { onAuthStateChanged } from "firebase/auth";
