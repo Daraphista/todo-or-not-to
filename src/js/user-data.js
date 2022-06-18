c65f5d4e38c94f91bc1574bc3e47ea6c0bc48da3
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-18 12:33:27 +0800

    fix(getUserData): remove getUserData

--- src/js/user-data.js
+++ src/js/user-data.js
@@ -1,14 +1,3 @@
-// Get user data
-
-import { collection, query, onSnapshot } from "firebase/firestore";
-
-const getUserData = (database, path, callback) => {
-  const collectionQuery = query(collection(database, path));
-  const unsub = onSnapshot(collectionQuery, callback);
-};
-
-export { getUserData };
-
 // set user data if user is new or doesn't have data
 
 import { database } from "./firebase";
