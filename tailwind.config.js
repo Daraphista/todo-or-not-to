a558171a68508cd03a786fd1dca73a32841f6c2b
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-20 23:45:32 +0800

    feat(keyframes): create grow keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        
+        grow: {
+          "0%": { transform: "scale(0)" },
+          "100%": { transform: "scale(1)" },
+        },
       }
     },
   },
