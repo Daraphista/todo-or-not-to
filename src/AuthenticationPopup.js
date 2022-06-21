af86bcb09da138fe57bb95d1a61d04fabacdc548
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-21 15:44:15 +0800

    feat(AuthenticationPopup): embed Background component

--- src/AuthenticationPopup.js
+++ src/AuthenticationPopup.js
@@ -23,6 +23,8 @@ const Background = () => {
 
 const AuthenticationPopup = () => {
   return (
-    <div className="authentication fixed hidden h-full w-full items-center justify-center bg-[#2C2B2B] text-white transition-opacity duration-200"></div>
+    <div className="authentication fixed hidden h-full w-full items-center justify-center bg-[#2C2B2B] text-white transition-opacity duration-200">
+      <Background />
+    </div>
   );
 };
