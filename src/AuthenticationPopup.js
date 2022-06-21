561d719f9c0986202394a5b7248b4206fb98f271
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-21 16:01:44 +0800

    feat(Background): change image sources

--- src/AuthenticationPopup.js
+++ src/AuthenticationPopup.js
@@ -8,19 +8,19 @@ import blob6 from "./assets/images/blob-6.svg";
 const Background = () => {
   return (
     <div className="background grid grid-cols-[448px_448px] absolute z-[-1]">
-      <img src="./assets/images/blob-3.svg" className="scale-0 animate-[grow_250ms] animation-fill-forwards" alt="blob 3" />
+      <img src={blob3} className="scale-0 animate-[grow_250ms] animation-fill-forwards" alt="blob 3" />
       <div className="top-left-blobs relative">
-        <img src="./assets/images/blob-4.svg" className="scale-0 animate-[grow_250ms_200ms] animation-fill-forwards"
+        <img src={blob4} className="scale-0 animate-[grow_250ms_200ms] animation-fill-forwards"
           alt="blob 4" />
         <img src={blob1}
           className="scale-0 animate-[grow_250ms_400ms] animation-fill-forwards absolute bottom-[-10%]" alt="blob 1" />
-        <img src="./assets/images/blob-2.svg"
+        <img src={blob2}
           className="scale-0 animate-[grow_250ms_500ms] animation-fill-forwards absolute right-[40%] bottom-[-10%]"
           alt="blob 2" />
       </div>
-      <img src="./assets/images/blob-5.svg" className="scale-0 animate-[grow_250ms_300ms] animation-fill-forwards"
+      <img src={blob5} className="scale-0 animate-[grow_250ms_300ms] animation-fill-forwards"
         alt="blob 5" />
-      <img src="./assets/images/blob-6.svg" className="scale-0 animate-[grow_250ms_100ms] animation-fill-forwards"
+      <img src={blob6} className="scale-0 animate-[grow_250ms_100ms] animation-fill-forwards"
         alt="blob 6" />
     </div>
   )
