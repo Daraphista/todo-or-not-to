ff975dd0e52f899e54542ce0cd1c5a7079881e86
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-21 15:42:45 +0800

    fix(Background): change class attributes to className

--- src/AuthenticationPopup.js
+++ src/AuthenticationPopup.js
@@ -1,19 +1,19 @@
 const Background = () => {
   return (
-    <div class="background grid grid-cols-[448px_448px] absolute z-[-1]">
-      <img src="./assets/images/blob-3.svg" class="scale-0 animate-[grow_250ms] animation-fill-forwards" alt="blob 3" />
-      <div class="top-left-blobs relative">
-        <img src="./assets/images/blob-4.svg" class="scale-0 animate-[grow_250ms_200ms] animation-fill-forwards"
+    <div className="background grid grid-cols-[448px_448px] absolute z-[-1]">
+      <img src="./assets/images/blob-3.svg" className="scale-0 animate-[grow_250ms] animation-fill-forwards" alt="blob 3" />
+      <div className="top-left-blobs relative">
+        <img src="./assets/images/blob-4.svg" className="scale-0 animate-[grow_250ms_200ms] animation-fill-forwards"
           alt="blob 4" />
-        <img src="./assets/images/blob-1.svg"
-          class="scale-0 animate-[grow_250ms_400ms] animation-fill-forwards absolute bottom-[-10%]" alt="blob 1" />
+        <img src={blob1}
+          className="scale-0 animate-[grow_250ms_400ms] animation-fill-forwards absolute bottom-[-10%]" alt="blob 1" />
         <img src="./assets/images/blob-2.svg"
-          class="scale-0 animate-[grow_250ms_500ms] animation-fill-forwards absolute right-[40%] bottom-[-10%]"
+          className="scale-0 animate-[grow_250ms_500ms] animation-fill-forwards absolute right-[40%] bottom-[-10%]"
           alt="blob 2" />
       </div>
-      <img src="./assets/images/blob-5.svg" class="scale-0 animate-[grow_250ms_300ms] animation-fill-forwards"
+      <img src="./assets/images/blob-5.svg" className="scale-0 animate-[grow_250ms_300ms] animation-fill-forwards"
         alt="blob 5" />
-      <img src="./assets/images/blob-6.svg" class="scale-0 animate-[grow_250ms_100ms] animation-fill-forwards"
+      <img src="./assets/images/blob-6.svg" className="scale-0 animate-[grow_250ms_100ms] animation-fill-forwards"
         alt="blob 6" />
     </div>
   )
