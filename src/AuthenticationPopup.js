33b14a7d401bec1766a35238ed5d2ad5d5aced57
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-22 10:45:14 +0800

    feat(AuthenticationPopup): use auth state

--- src/AuthenticationPopup.js
+++ src/AuthenticationPopup.js
@@ -30,6 +30,8 @@ import { useAuthState } from "react-firebase-hooks";
 import { auth } from "./firebase"
 
 const AuthenticationPopup = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <div className="authentication fixed hidden h-full w-full items-center justify-center bg-[#2C2B2B] text-white transition-opacity duration-200">
       <Background />
