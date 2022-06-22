ed2cb443e55b6b040ccaabde3bff73c3de11dee8
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-22 13:31:32 +0800

    feat(SignUpForm): use create user with email and password

--- src/AuthenticationPopup.js
+++ src/AuthenticationPopup.js
@@ -132,6 +132,9 @@ const SignInForm = () => {
 };
 
 const SignUpForm = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
   return (
     <form className="sign-up animation-fill-forwards hidden min-w-[25rem] scale-0 animate-[grow_250ms_600ms] flex-col gap-8 rounded-xl bg-white/[0.05] px-8 py-9 text-center shadow-lg backdrop-blur-lg">
       <h1 className="text-4xl">Sign In</h1>
