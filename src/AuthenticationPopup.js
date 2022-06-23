bc29f4484d0dd09a4044a358205b6ccd97bc2881
daraphista <angeloraphaelmendoza@gmail.com>
2022-06-23 12:20:19 +0800

    fix(SignInForm): center loading indicator

--- src/AuthenticationPopup.js
+++ src/AuthenticationPopup.js
@@ -129,7 +129,7 @@ const SignInForm = (props) => {
           onInput={(e) => setPassword(e.target.value)}
         />
       </label>
-      <button className="sign-in flex-1 rounded-md bg-[#393939]/[0.8] py-2">
+      <button className="sign-in flex justify-center flex-1 rounded-md bg-[#393939]/[0.8] py-2">
         {loading ? (
           <img className="h-6" src={loadingIcon} alt="loading" />
         ) : (
